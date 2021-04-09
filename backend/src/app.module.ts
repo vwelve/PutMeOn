c57d431e970ab57cfc8fe78fd45f82442c3b0449
Dominic Humber <dhumber2017@gmail.com>
2021-04-09 19:45:27 -0400

    Initiate NestJs Template

---
+++ backend/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [GraphQLModule.forRoot({
+    debug: false,
+    playground: false,
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
