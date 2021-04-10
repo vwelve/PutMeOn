83783878d562fce4df645ac5c77bd632d8b39acf
Dominic Humber <dhumber2017@gmail.com>
2021-04-09 20:51:49 -0400

    Add authentication

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [GraphQLModule.forRoot({
     debug: false,
     playground: false,
-  })],
+  }), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
